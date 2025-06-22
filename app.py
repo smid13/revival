6a2d26ead580466e43261609270c5a18c88b7033
smid13 <84406168+smid13@users.noreply.github.com>
2025-06-22 14:01:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
+
+db_url = os.environ.get("SQLALCHEMY_DATABASE_URI")
+if not db_url:
+    raise RuntimeError("Chybí proměnná SQLALCHEMY_DATABASE_URI")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 #app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 #db = SQLAlchemy(app)
