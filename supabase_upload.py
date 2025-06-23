3f11f0daf3b966ff72e10c1f629aca1c0fa57539
smid13 <84406168+smid13@users.noreply.github.com>
2025-06-23 19:24:31 +0200

    Update supabase_upload.py

--- supabase_upload.py
+++ supabase_upload.py
@@ -6,7 +6,7 @@ SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 BUCKET_NAME = "qr"
 
 def upload_qr_to_supabase(file_path, filename):
-    upload_url = f"{SUPABASE_URL}/storage/v1/{filename}"
+    upload_url = f"{SUPABASE_URL}/storage/v1/object/qr/{filename}"
     headers = {
         "apikey": SUPABASE_API_KEY,
         "Authorization": f"Bearer {SUPABASE_API_KEY}",
@@ -18,7 +18,7 @@ def upload_qr_to_supabase(file_path, filename):
 
     if res.status_code in [200, 201]:
         # Veřejná URL k souboru
-        return f"{SUPABASE_URL}/storage/v1/{filename}"
+        return f"{SUPABASE_URL}/storage/v1/object/qr/{filename}"
     else:
         print(f"Chyba při nahrávání: {res.status_code} - {res.text}")
         return None
